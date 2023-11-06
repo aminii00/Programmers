89a0e47b9e32213a2df67be68ee9a66419a01c6a
m0503 <m0503@LAPTOP-9LSAAAVT>
2023-11-06 14:22:57 +0900

    프로그래머스 문제풀이

---
+++ Programmers/src/Test.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		/*문자열 거꾸로 출력*/
+		String str = "Hello";
+		for(int i=str.length()-1; i>=0; i--) {
+			System.out.print(str.charAt(i));
+		}
+		
+	}
+		
+		
+	}
